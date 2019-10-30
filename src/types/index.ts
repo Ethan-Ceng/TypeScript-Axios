f871c2cc2dcb9ab1520a59b166436be1ef01ac1e
ZengFuming <ZengFuming@hdvon.com>
2019-10-30 18:18:35 +0800

    axios request, response Interceptor

--- src/types/index.ts
+++ src/types/index.ts
@@ -44,6 +44,10 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
+  interceptor: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>
+    response: AxiosResponse<AxiosResponse>
+  }
   request<T = any>(config?: AxiosRequestConfig): AxiosPromise<T>
 
   get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
@@ -65,4 +69,18 @@ export interface AxiosInstance extends Axios {
   <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
   <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosInterceptorManager<T> {
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number
+
+  eject(id: number): void
+}
+
+export interface ResolvedFn<T> {
+  (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+  (error: any): any
 }
